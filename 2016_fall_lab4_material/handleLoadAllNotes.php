c55d9b2cbf37604c9e8fd27690ceb3d86670660f
djp <duanjp8617@gmail.com>
2016-10-31 22:02:18 +0800

    prof. Wu's lab4 material

--- 2016_fall_lab4_material/handleLoadAllNotes.php
+++ 2016_fall_lab4_material/handleLoadAllNotes.php
@@ -1,19 +1,19 @@
 <?php
     
-    $conn=mysqli_connect('sophia.cs.hku.hk','jpduan','dj824135') or die ('Failed to Connect '.mysqli_error($conn));
+  $conn=mysqli_connect('sophia.cs.hku.hk','username','pwd') or die ('Failed to Connect '.mysqli_error($conn));
 
-    mysqli_select_db($conn,'jpduan') or die ('Failed to Access DB'.mysqli_error($conn));
-	
-    $query = 'select * from notes';
-	
-    $result = mysqli_query($conn, $query) or die ('Failed to query '.mysqli_error($conn));
+  mysqli_select_db($conn,'dbname') or die ('Failed to Access DB'.mysqli_error($conn));
+  
+  $query = 'select * from notes';
+  
+  $result = mysqli_query($conn, $query) or die ('Failed to query '.mysqli_error($conn));
 
-    $json = array();
-	
-    while($row=mysqli_fetch_array($result)) {
-        $json[]=array('id'=>$row['id'], 'title'=>$row['title'],'content'=>$row['content']);
-    }
+  $json = array();
+  
+  while($row=mysqli_fetch_array($result)) {
+    $json[]=array('id'=>$row['id'], 'title'=>$row['title'],'content'=>$row['content']);
+  }
 
-    print json_encode(array('notes'=>$json));
+  print json_encode(array('notes'=>$json));
 
 ?>
--- 2016_fall_lab4_material/script.js
+++ 2016_fall_lab4_material/script.js
@@ -1,175 +1,227 @@
 $(document).ready(function() {
-	// TODO: 1. Call loadAllNotes function to load 
-	//          all available notes from the database.
-	loadAllNotes();
 
-	// TODO: 2. Set the "click" event handler of "#buttonSave" button to saveNewNote function
-	$("#buttonSave").on("click", saveNewNote);
+  //Call loadAllNotes() function to load all available notes from the database.
+  loadAllNotes();
 
-	// TODO: 3. Set the "click" event handler of "#buttonUpdate" button to updateNote function
-	$("#buttonUpdate").on("click", updateNote);
+  // Set event handler of "click" on "#buttonSave" button to saveNewNote() function
+  $("#buttonSave").on("click", saveNewNote);
 
-	// TODO: 4. show "#buttonSave" element
-	$("#buttonSave").show();
-	
-	// TODO: 5. hide "#buttonUpdate" element
-	$("#buttonUpdate").hide();
+  // TODO: 1. Set event handler of "click" on "#buttonUpdate" button to saveNewNote() function
+  
+  // TODO: 2. show the "#buttonSave" button
+  
+  // TODO: 3. hide the "#buttonUpdate" button
 
 });
 
 function loadAllNotes(){
-	// Since other functions may call loadAllNotes() to 
-	// retrieve all the notes again, so we need to remove the html content of the
-	// "#ulNotes" element and clear all the input boxes on the webpage.
+  // Use $.getJSON to retrive a json string response from "handleLoadAllNotes.php".
+  // The json string stores all the notes in the following format:
+  // "notes":[{"id":x, "title":y, "content":z}, {"id":x1, "title":y1, "content":z1}]
+  $.getJSON("handleLoadAllNotes.php", function(jsonData){
+    
+    // The notes should be displayed with their titles sorted in ascending alphabatical order.
+    // We use "sort" to sort the jsonData.notes array according to the "title" field of
+    // each object in the array.
+    // The arguments of the anonymous function x and y represent 2 objects in the array that
+    // are to be sorted.   
+    jsonData.notes.sort(function(x, y){
+      
+      // Transform the title field of x and y to upper case using toUpperCase() 
+      x_title = x["title"].toUpperCase();
+      y_title = y["title"].toUpperCase();
 
-	// TODO: 1. Change the html content of "#ulNotes" element to ""
-	$("#ulNotes").html("");
-
-	// TODO: 2. Change the value of "#noteTitle" and "#noteContent" input boxes to ""
-	$("#noteTitle").val("");
-	$("#noteContent").val("");
-
-	// TODO: 3. Use getJSON to retrive a json string response from "handleLoadAllNotes.php".
-	//          The json string stores all the simplified notes using the following format:
-	//          "notes":[{"id":x, "title":y, "content":z}, {"id":x1, "title":y1, "content":z1}]
-	$.getJSON("handleLoadAllNotes.php", function(jsonData){
-		
-
-		// The notes should be displayed with their titles sorted in ascending alphabatical order.
-		// So we need to sort the jsonData.notes array according to the "title" field of
-		// each object in the array.
-		// The argument of the anonymous function x and y represent 2 objects in the array that
-		// are waiting to be sorted.   
-		jsonData.notes.sort(function(x, y){
-			
-			// TODO: 4. Transform the title field of x and y to upper case using toUpperCase() method
-			//          and save the transformed strings in x_title and y_title.
-			x_title = x["title"].toUpperCase();
-			y_title = y["title"].toUpperCase();
-
-			// TODO: 5. Return -1, 1 or 0 according to the comparing result between x_title and y_title.
-			if (x_title < y_title){
-        		return -1;
-        	} 
-    		if (x_title > y_title){
-        		return 1;
-        	}
-        	else{
-    			return 0;
-    		} 
-		});
-
-		// TODO: 6. For each note contained in the json string, create a <li> element and append this <li> element 
-		//          to "#ulNotes" element.
-		//          If the id, title and content field of a note is 3, "Hello" and "Yeah!" respectively, then
-		//          the format of the <li> element should be:
-		//          <li><h3 class="clickableTitle" noteId=3>Hello</h3><h4>Yeah!</h4></li>
-		$.each(jsonData.notes, function(i, note){
-
-			var txt = "";
-			txt += "<li>";
-			txt += "<h3 class=\"clickableTitle\" noteId="+note["id"]+" id=h3_"+note["id"]+">"+note["title"]+"</h3>";
-			txt += "<h4 id=h4_"+note["id"]+">"+note["content"]+"</h4>";
-			txt += "</li>";
-
-			$("#ulNotes").append(txt);
-		});
+      if (x_title < y_title){
+        return -1;
+      } 
+      if (x_title > y_title){
+        return 1;
+      }
+      
+      return 0;
+    });
+    
+    $.each(jsonData.notes, function(i, note){
+      // TODO: 4. For each note contained in the sorted json string, create a <li> element to contain the note info. 
+      //          If the id, title and content field of a note is 3, "Hello" and "Yeah!" respectively, 
+      //          the format of the <li> element should be:
+      //          <li><h3 class="clickableTitle" noteId=3>Hello</h3><h4>Yeah!</h4></li>
+      //          append this <li> element to the end of the "#ulNotes" element.
 
 
-		// TODO: 7. Remember in TODO 6 that the class attribute of <h3> element containing the title of the note 
-		//          is set to "clickableTitle".
-		//          Here we need set the click event handler of all the elements whose class attributes are
-		//          "clickableTitle" to startUpdate function
-		$(".clickableTitle").on("click", startUpdate);
-	});
+
+      
+    });
+
+    // set the event handler of "click" on all the elements whose class attributes are
+    // "clickableTitle" to showUpdate() function
+    $(".clickableTitle").on("click", showUpdate);
+
+  });
 }
 
 function saveNewNote(){
-	// This function is triggered when save button is clicked.
-	// A new note is generated and saved in the database.
+  // This function is triggered when "Save" button is clicked.
+  // A new note is generated, saved to the database, and displayed on the page.
 
-	// TODO: 1. Retrieve the title of the new note from the value of "#noteTitle" input box.
-	var newTitle = $("#noteTitle").val();
+  // TODO: 5. Retrieve the title of the new note from the value of "#noteTitle" input box.
+  var newTitle =            ;
 
-	// TODO: 2. Retrieve the content of the new note from the value of "#noteContent" input box.
-	var newContent = $("#noteContent").val();
+  // TODO: 6. Retrieve the content of the new note from the value of "#noteContent" input box.
+  var newContent =           ;
 
 
-	// TODO: 3. Generate an HTTP GET request using $.get function to handleNewNote.php file.
-	//          The generated HTTP GET request should carry the following 2 key-value pairs.
-	//          "title":title of the new note
-	//          "content":content of the new note
-	//          In the response handler, if the response text is "newNoteOK", reload the webpage by calling
-	//          loadAllNotes(). Otherwise, generate an alert box, with "New note is not successfully created."
-	//          as the information        
-	$.get("handleNewNote.php?title="+newTitle+"&content="+newContent, function(data, status){
-		if(data=="newNoteOK"){
-			loadAllNotes();
-		}
-		else{
-			alert("New note is not successfully created.");
-		}
-	});
+  // TODO: 7. Generate an AJAX GET request using $.get() to retrieve handleNewNote.php file.
+  //          The GET request should carry the following 2 key-value pairs.
+  //          "title":title of the new note
+  //          "content":content of the new note  
+  $.get(                                , function(data, status){
+      
+    var txt = "";
+    // TODO: 8. generate a <li> element to contain the new note  
+    //          in the same format as in TODO 4 and store it in txt
+    
+    
+    
+    
+    
+    
+    //the following codes insert the new note into the notes list on the page 
+    //according to alphabetic order 
+    var insertedNewNote=false;
+    
+    var oldnotes=$("#ulNotes").children();
+    $.each(oldnotes, function(){
+      //insert the new note before the note whose title comes 
+      //after the the new note's title alphabetically
+      if ($(this).find("h3").html().toUpperCase() > newTitle.toUpperCase()){
+        $(this).before(txt);
+        insertedNewNote=true;
+        
+        //after inserting, break from the $.each() loop
+        return false;
+      }
+    });
+    
+    //if titles of all existing notes are alphabetically ordered before the new note's title,
+    //insert the new note at the end of the notes list
+    if (!insertedNewNote){
+      $("#ulNotes").append(txt);
+    }
+    
+    // set the event handler of "click" on all the elements whose class attributes are
+    // "clickableTitle" to showUpdate() function
+    $(".clickableTitle").on("click", showUpdate);
+    
+    // TODO: 9. Change the values of "#noteTitle" and "#noteContent" input boxes to ""
+
+  
+  });
 }
 
-function startUpdate(){
-	// This function is called when some on click on the <h3> element
-	// containing the title of the note. 
+function showUpdate(){
+  // This function is called when one clicks on the <h3> element
+  // containing the title of a note. 
+
+  // TODO: 10. Retrieve the title from the <h3> element being clicked (Hint: use $(this)) 
+  //           and display it in the "#noteTitle" input box.
+  
+  
+  
+  // TODO: 11. Retrieve the content from the next sibling element (Hint: use .next()) of the 
+  //           <h3> element being clicked and display it in the "#noteContent" input box
+  
+  
+  
+  // TODO: 12. change value of the "noteId" attribute of the "#noteTitle" element to the id of the note whose title is being clicked
+  //           Hint: The id of the note is stored as the value of "noteId" attribute
+  //           of the <h3> element being clicked.
+  
+  
+  
+  // TODO: 13. Hide "#buttonSave" element. Show "#buttonUpdate" element. 
+  //           Change the content of "#rightHeader" element to "Update Note".
 
 
-	// TODO: 1. Retrieve the title from the text field of the <h3> element being clicked 
-	//          and save it to the value field of "#noteTitle" input box.
-	$("#noteTitle").val($(this).text());
 
-	// TODO: 2. Retrieve the content from the text field of the next sibling element of the 
-	//          <h3> element being clicked and save it to the value field of "#noteContent" input box
-	$("#noteContent").val($(this).next().text());
-
-	// TODO: 3. change the "noteId" attribute of "#noteTitle" element to the id of the note that 
-	//          the user wants to update. 
-	//          Hint: The id of the note that the user wants to update is stored as the value of "noteId" attribute
-	//                of the <h3> element being clicked.
-	$("#noteTitle").attr({"noteId":$(this).attr("noteId")});
-
-	// TODO: 4. Hide "#buttonSave" element. Show "#buttonUpdate" element. 
-	//          Change the text field of "#rightHeader" element to "UpdateOldNote".
-	$("#buttonSave").hide();
-	$("#buttonUpdate").show();
-	$("#rightHeader").html("Update Old Note");
 }
 
 function updateNote(){
-	// This function is triggered when the update button is clicked when displaying the 
-	// full note
+  // This function is triggered when the "Update" button is clicked 
 
-	// TODO: 1. Retrieve the updated title from the value of "#noteTitle" input box
-	var newTitle = $("#noteTitle").val();
+  // TODO: 14. Retrieve the updated title from the value of "#noteTitle" input box
+  var newTitle =         ;
 
-	// TODO: 2. Retrieve the updated content from the value of "#newContent" input box
-	var newContent = $("#noteContent").val();
+  // TODO: 15. Retrieve the updated content from the value of "#newContent" input box
+  var newContent =       ;
 
-	// TODO: 3. Retrieve the id of the note from the "noteId" attribute of the "#noteTitle" input box
-	var id = $("#noteTitle").attr("noteId");
+  // TODO: 16. Retrieve the id of the note from the "noteId" attribute of the "#noteTitle" input box
+  var id =        ;
 
 
-	// TODO: 4. Generate an HTTP GET request using $.get function to handleUpdate.php file.
-	//          The generated HTTP GET request should carry the following 3 key-value pairs.
-	//          "title":updated title of the note
-	//          "content":updated content of the note
-	//          "id":id of the note to be updated
-	//          In the response handler, if the response text is "updateSucceed", show "#buttonSave" element,
-	//          hide "#buttonUpdate" element, change the text field of "#rightHeader" to "Add New Note" and then
-	//          call loadAllNotes() to refect the updates.
-	$.get("handleUpdate.php?title="+newTitle+"&content="+newContent+"&id="+id, function(data, status){
-		if(data=="updateSucceed"){
-			$("#buttonSave").show();
-			$("#buttonUpdate").hide();
-			$("#rightHeader").html("Add New Note");
+  // TODO: 17. Generate an AJAX GET request using $.get() to retrieve handleUpdate.php.
+  //           The generated GET request should carry the following 3 key-value pairs.
+  //           "title":updated title of the note
+  //           "content":updated content of the note
+  //           "id":id of the note to be updated
+  $.get(          , function(data, status){
+    if(data=="updateSuccess"){
 
-			loadAllNotes();
-		}
-		else{
-			alert("The note is not successfully updated.");
-		}
-	});
+      //  TODO: 18. Show "#buttonSave" element. Hide "#buttonUpdate" element. 
+      //            Change the content of "#rightHeader" element to "Add New Note".
+      //            Empty contents in "#noteTitle" and "#noteContent" input fields 
+      
+      
+      
+      
+      
+      
+      
+      var txt = "";
+      // TODO: 19. generate a <li> element to contain the new note  
+      //           in the same format as in TODO 4 and store it in txt
+      
+      
+      
+      
+      
+      
+      //the following codes remove the old note and insert the updated note 
+      //into the notes list according to alphabetic order       
+      var removedOldNote=false;
+      var insertedUpdatedNote=false;
+      
+      var oldnotes=$("#ulNotes").children();
+      $.each(oldnotes, function(){
+        if (!removedOldNote && $(this).find("h3").attr("noteId") == id){
+          //remove the old note
+          $(this).remove();
+          removedOldNote = true;
+        }else{ 
+          if (!insertedUpdatedNote && $(this).find("h3").html().toUpperCase() > newTitle.toUpperCase()){  
+              //insert the updated note to the correct position
+              $(this).before(txt);
+              insertedUpdatedNote=true;           
+          }
+        }
+        
+        if (removedOldNote && insertedUpdatedNote){
+          //break from the $.each() loop
+          return false;
+        }       
+      });
+      
+      //insert the updated note to the correct position
+      if (!insertedUpdatedNote){
+        $("#ulNotes").append(txt);
+      }
+      
+      // set the event handler of "click" on all the elements whose class attributes are
+      // "clickableTitle" to showUpdate() function
+      $(".clickableTitle").on("click", showUpdate);
+    }
+    else{
+      alert("The note is not successfully updated.");
+    }
+  });
 }
