6a2959a7b5ddf923eda151f9c5d5be561238a042
djp <duanjp8617@gmail.com>
2016-11-17 16:20:23 +0800

    finish programming of lab7

--- lab7/public/javascripts/externalJS.js
+++ lab7/public/javascripts/externalJS.js
@@ -2,6 +2,47 @@
 
 var studentListApp = angular.module('studentList', []);
 
-studentListApp.controller("StudentListController", function StudentListController($scope){
-    $scope.existing_student = {name:'fk', major:'fk', gender:'fk', studentid:'fk'};
+studentListApp.controller("StudentListController", function StudentListController($scope, $http){
+    $scope.existing_student = {name:"", email:"", major:"", gender:"", studentID:"", _id:""};
+
+    var getStudents = function(){
+      $http.get("/users/studentList").then(function(response){
+        $scope.students = response.data;
+      }, function(response){
+        alert("Error getting students.");
+      });
+    };
+
+    getStudents();
+
+    $scope.showStudent = function(student){
+      $scope.existing_student = student;
+    };
+
+    $scope.deleteStudent = function(id){
+      var url = "/users/deletestudent/"+id;
+      $http.delete(url).then(function(response){
+        if(response.data.msg===''){
+          getStudents();
+        }
+        else{
+          alert("Error deleting students.");
+        }
+      }, function(response){
+        alert("Error deleting students.");
+      });
+    };
+
+    $scope.addStudent = function(student){
+      $http.post("/users/addstudent", student).then(function(response){
+        if(response.data.msg===''){
+          getStudents();
+        }
+        else{
+          alert("Error adding student.");
+        }
+      }, function(response){
+        alert("Error adding student.");
+      });
+    };
 })
